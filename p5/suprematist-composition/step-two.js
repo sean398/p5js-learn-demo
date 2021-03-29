66dd81c88f711a12e432e0ddc10e8f4e9c0326f3
Sean398 <guohaochuan5461@gmail.com>
2021-03-30 01:06:29 +0800

    add step three

--- p5/suprematist-composition/step-two.js
+++ p5/suprematist-composition/step-two.js
@@ -11,12 +11,18 @@ function draw() {
 
 function drawRandomQuads() {
   for (let i = 0; i < 12; i++) {
+    const randomStrokeWeight = random(5);
+    strokeWeight(randomStrokeWeight);
+
     if (i < 4) {
       fill(108, 148, 196); // blue
+      stroke(108, 148, 196); // blue
     } else if (i >= 4 && i < 8) {
       fill(253, 253, 139); //yellow
+      stroke(253, 253, 139); //yellow
     } else {
       fill(255, 105, 97); //red
+      stroke(255, 105, 97); //red
     }
     const deviate = random(30);
     approxQuad(deviate);
@@ -32,17 +38,32 @@ function approxQuad(deviate) {
   const height = random(300);
   const centerX = windowWidth / 2 + random(-300, 300);
   const centerY = windowHeight / 2 + random(-300, 300);
+  // get random shape name
+  const shape = getRenderName(); //["quad", "line", "ellipse", "arc"]
 
-  quad(
-    centerX - width,
-    centerY - height,
-    centerX + width + random(-deviate, deviate),
-    centerY - height + random(-deviate, deviate),
-    centerX + width + random(-deviate, deviate),
-    centerY + height + random(-deviate, deviate),
-    centerX - width + random(-deviate, deviate),
-    centerY + height + random(-deviate, deviate)
-  );
+  if (shape === "quad") {
+    quad(
+      centerX - width,
+      centerY - height,
+      centerX + width + random(-deviate, deviate),
+      centerY - height + random(-deviate, deviate),
+      centerX + width + random(-deviate, deviate),
+      centerY + height + random(-deviate, deviate),
+      centerX - width + random(-deviate, deviate),
+      centerY + height + random(-deviate, deviate)
+    );
+  } else if (shape === "line") {
+    line(randX(), randY(), randX(), randY());
+  } else if (shape === "ellipse") {
+    const ellipseWidth = random(50, 60);
+    const ellipseHeight = random(20, 40);
+    ellipse(centerX, centerY, ellipseWidth, ellipseHeight);
+  } else if (shape === "arc") {
+    const arcWidth = random(200);
+    const start = random() * TWO_PI;
+    const end = random() * TWO_PI;
+    arc(centerX, centerY, arcWidth, arcWidth, start, end);
+  }
 }
 
 // use these two functions to get random numbers that don't draw on the sides of the screen
@@ -52,4 +73,9 @@ function randX() {
 
 function randY() {
   return random(100, height - 100);
+}
+
+function getRenderName() {
+  let words = ["quad", "line", "ellipse", "arc"];
+  return (word = random(words));
 }
